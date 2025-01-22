40da6539d0ee42d5cf88530a20cc00fe4b1f487f
altie122 <altielynn@gmail.com>
2025-01-22 08:28:10 -0700

    add dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import db from '@astrojs/db';
+import mdx from '@astrojs/mdx';
+import clerk from '@clerk/astro';
+
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), tailwind(), db(), mdx()],
+  adapter: cloudflare(),
+  output: 'server',
+});
