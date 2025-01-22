ab694984e492f9c0977a2029fd833250fadde258
altie122 <altielynn@gmail.com>
2025-01-22 09:41:37 -0700

    fix DB seed

--- db/seed.ts
+++ db/seed.ts
@@ -9,7 +9,8 @@ export default async function seed() {
 	const tags = [
 		{ name: 'sports', id: "1", posts: "1" },
 		{ name: 'studentlife', id: "2", posts: "0" },
-	]
+	];
 	const posts = [
 		{ headline: 'First post', secondaryHeadline: 'This is the first post', authorId: "1", content: 'This is the first post', tag: "1", id: "1", published: true, reviewed: false, createdAt: new Date() },
+	];
 }
