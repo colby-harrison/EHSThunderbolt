3e5389491ee21cbaa82bc0a5ddcf3e16c8617025
altie122 <altielynn@gmail.com>
2025-01-23 16:30:53 -0700

    test react+cloudflare fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  vite: {
+    resolve: {
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      alias: import.meta.env.PROD && {
+        "react-dom/server": "react-dom/server.edge",
+      },
+    },
+  },
 });
