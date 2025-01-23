bbfe38e461fa8e44a8c4b38b0db9d04776ef9c33
altie122 <altielynn@gmail.com>
2025-01-23 09:24:24 -0700

    test PNPM not NPM

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-import db from "@astrojs/db";
-import mdx from "@astrojs/mdx";
-// import clerk from "@clerk/astro";
-import cloudflare from "@astrojs/cloudflare";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://ehsthunderbolt.org",
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    db(),
-    mdx(),
-    // clerk(),
-  ],
-  adapter: cloudflare(),
-  output: "static",
-});
+export default defineConfig({});
