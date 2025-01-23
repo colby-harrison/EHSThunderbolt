9dad5a945f7c2a1934efe851a537db9c66ea532b
altie122 <altielynn@gmail.com>
2025-01-23 10:52:07 -0700

    add db shema and seed back

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,36 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column } from 'astro:db';
 
 // https://astro.build/db/config
+// database schema
 export default defineDb({
-  tables: {}
+  tables: {
+    posts: defineTable({
+      columns: {
+        id: column.text({ primaryKey: true, default: crypto.randomUUID() }),
+        headline: column.text(),
+        secondaryHeadline: column.text(),
+        authorId: column.text(),
+        content: column.text(),
+        tag: column.text(),
+        published: column.boolean({ default: false }),
+        reviewed: column.boolean({ default: false }),
+        createdAt: column.date({ default: new Date() }),
+      }
+    }),
+    authors: defineTable({
+      columns: {
+        id: column.text({ primaryKey: true, default: crypto.randomUUID() }),
+        clerkId: column.text(),
+        name: column.text(),
+        admin: column.boolean({ default: false }),
+      }
+    }),
+    tags: defineTable({
+      columns: {
+        id: column.text({ primaryKey: true, default: crypto.randomUUID() }),
+        name: column.text(),
+        posts: column.text(),
+      }
+    }),
+  }
 });
