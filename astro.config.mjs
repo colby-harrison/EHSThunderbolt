74b9cb9be1d4e38418dfeb7361bce02baf0d1131
altie122 <altielynn@gmail.com>
2025-01-23 09:32:10 -0700

    no longer use the cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import cloudflare from '@astrojs/cloudflare';
+// import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  adapter: cloudflare(),
+  // adapter: cloudflare(),
 });
