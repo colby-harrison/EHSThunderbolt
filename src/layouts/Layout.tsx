bb5acbf93cb7a2da3c1c78b50c0812609f78def2
altie122 <77561831+altie122@users.noreply.github.com>
2025-01-24 14:10:58 -0700

    Add navbar and sidebar (#1)
    * add side bar and name var, needs a bit more formatting
    
    * make side bar say EHS not BCUW
    
    * make sidebar hide
    
    * got the layout to look good, added a few assets, changed some colors, and made a few elements on the home page
    
    * fix a file name

---
+++ src/layouts/Layout.tsx
@@ -0,0 +1,29 @@
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/AppSidebar";
+import React from "react";
+import Navbar from "@/components/Navbar";
+// import Progress from "./progressbar";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  const [open, setOpen] = React.useState(() => {
+    const savedOpenState = sessionStorage.getItem("main-sidebarOpen");
+    return savedOpenState ? JSON.parse(savedOpenState) : false;
+  });
+  React.useEffect(() => {
+    sessionStorage.setItem("main-sidebarOpen", JSON.stringify(open));
+  }, [open]);
+  return (
+    <SidebarProvider open={open} onOpenChange={setOpen}>
+      {/* <Progress /> */}
+      <main className='w-full h-full'>
+        <Navbar />
+        <div className='h-[calc(100vh-3rem)] flex flex-row'>
+        <AppSidebar />
+        <div className="w-full h-full p-2 overflow-y-scroll overflow-x-hidden">
+        {children}
+        </div>
+        </div>
+      </main>
+    </SidebarProvider>
+  );
+}
