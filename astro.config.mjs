ea8211a4095d199d120be1d27381c1aac5c18f18
altie122 <altielynn@gmail.com>
2025-01-29 12:32:19 -0700

    switch ssr adapter to verc el

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,26 +2,21 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import cloudflare from "@astrojs/cloudflare";
 
 import db from "@astrojs/db";
+
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), db()],
   output: "server",
-  adapter: cloudflare(),
+
   vite: {
-    resolve: {
-      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
-      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
-      // @ts-expect-error "env works, just ts intelisense doesn't like it"
-      alias: import.meta.env.PROD && {
-        "react-dom/server": "react-dom/server.edge",
-      },
-    },
     server: {
       allowedHosts: true,
     },
   },
+
+  adapter: vercel(),
 });
