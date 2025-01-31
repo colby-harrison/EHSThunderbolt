399c61a0b798004b4433c1d949167b36eb80a16c
altie122 <altielynn@gmail.com>
2025-01-30 18:28:56 -0700

    make comments in .env.example and middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,7 @@
+// middleware.ts | DO NOT TOUCH UNLESS YOU KNOW WHAT YOU ARE DOING
+// if you do not know what is going on in this file without comments
+// you should not be messing with this file
+
 import { clerkMiddleware } from '@clerk/astro/server';
 
 export const onRequest = clerkMiddleware();
