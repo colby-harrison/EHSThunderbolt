f1a80ccefc3d4eb211c6b0bfb363a7d48f6d1d42
altie122 <altielynn@gmail.com>
2025-01-31 11:17:17 -0700

    lint and format

--- src/pages/api/teachers/update.ts
+++ src/pages/api/teachers/update.ts
@@ -1,22 +1,23 @@
-import type { APIRoute } from "astro";
+import type { APIRoute } from 'astro';
 import data from '@/server/queries';
-import type { types } from "@/lib";
+import type { types } from '@/lib';
 
+// eslint-disable-next-line
 export const GET: APIRoute = async ({ params, request, locals }) => {
-  const { userId } = locals.auth()
+  const { userId } = locals.auth();
 
   if (!userId) {
-    return new Response('Error: No signed in user', { status: 401 })
+    return new Response('Error: No signed in user', { status: 401 });
   }
 
-  const teacherData = await request.formData()
+  const teacherData = await request.formData();
   const teacherObject: types.teacher = {
     id: Number(teacherData.get('id')),
     name: String(teacherData.get('name')),
     picture: String(teacherData.get('picture')),
     job: String(teacherData.get('job')),
-  }
-  await data.post.teachers.update(teacherObject.id, teacherObject)
+  };
+  await data.post.teachers.update(teacherObject.id, teacherObject);
 
-  return new Response('Success', { status: 200 })
-}
+  return new Response('Success', { status: 200 });
+};
