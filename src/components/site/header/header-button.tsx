c8dd6bf059cc2dbee5dfe2f13f0b68eb35d5b1e7
altie122 <altielynn@gmail.com>
2025-04-23 12:27:33 -0600

    small change to the header button

--- src/components/site/header/header-button.tsx
+++ src/components/site/header/header-button.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export function HeaderBtn({ text, href }: { text: string; href: string }) {
 	return (
-		<Button variant="outline" asChild>
+		<Button variant="outline" asChild className="hover:bg-ehs-blue/50">
 			<Link href={href}>{text}</Link>
 		</Button>
 	);
