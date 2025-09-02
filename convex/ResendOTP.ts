bd24ee9ab1640cfe7ea0b45ffba52b1468077fdc
Alton Rose <git@altie122.xyz>
2025-09-02 09:33:17 -0600

    Update ResendOTP.ts

--- convex/ResendOTP.ts
+++ convex/ResendOTP.ts
@@ -22,7 +22,7 @@ export const ResendOTP = Email({
     // if allowed, continue
     const resend = new ResendAPI(provider.apiKey);
     const { error } = await resend.emails.send({
-      from: "Thunderbolt <thunderbolt@email.thunderbolt.org>",
+      from: "Thunderbolt <thunderbolt@email.ehsthunderbolt.org>",
       to: [email],
       subject: `Sign in to EHS Thunderbolt`,
       text: `Your code is ${token}\n\nThis code will expire in 15 minutes.`,
